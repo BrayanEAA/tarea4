40323f47109d3a0746f1d2d6cb47b2e4c7e9a019
BrayanEAA <brayaneduardoarteaga@gmail.com>
2023-02-17 19:56:26 -0600

    Update Program.cs

--- tarea4/Program.cs
+++ tarea4/Program.cs
@@ -1,5 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 ﻿
+using tarea4.entidades;
 using tarea4.negocio;
+
+EntidadContador Texto = new EntidadContador ();
+ClsNegocioContador clsNegocioContador = new ClsNegocioContador();
+
+
+
+  Console.WriteLine("Ingrese un Texto:");
+  string textoIngresado = Console.ReadLine();
+
+  EntidadContador entidad = new EntidadContador();
+  entidad.Texto = textoIngresado;
+
+  ClsNegocioContador negocio = new ClsNegocioContador();
+  int cantidadLetras = negocio.ContarLetras(entidad);
+
   Console.WriteLine ("La cantidad de letras ingresadas es: " + cantidadLetras);
